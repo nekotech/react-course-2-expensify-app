7e6b5eb5ba6752d82f8e3dcefd4ee06eb0947577
Matthew Lee <leematthewrobert@yahoo.com.au>
2019-02-13 22:19:14 -0700

    Initial commit

---
+++ src/playground/destructuring.js
@@ -0,0 +1,47 @@
+//
+// Object destructuring
+//
+
+// const person = {
+//   name: 'Matt',
+//   age: 48,
+//   location: {
+//     city: 'Lafayette',
+//     temp: -3
+//   }
+// };
+
+// const {name: firstName = 'Anonymous', age} = person;
+
+// console.log(`${firstName} is ${age}`);
+
+// const {temp: temperature, city} = person.location;
+
+// if (city && temperature) {
+//   console.log(`It's ${temperature} in ${city}`)
+// }
+
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// };
+
+// const { name: publisherName = 'Self Published'} = book.publisher;
+
+// console.log(publisherName);
+
+//
+// Array Destructuring
+//
+const address = ['1299 South Juniper St', 'Philadelphia', 'Pennsylvania', '19147'];
+const [, city, state = 'New York', ] = address;
+console.log(`You are in ${city}, ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [drink, small, medium, large] = item;
+
+console.log(`A medium ${drink} costs ${medium}`);
